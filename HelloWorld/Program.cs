a1c10f672b4a1b810199b1e0a009b3b8bf6f3563
Fei Xu <fei.xu@infoflosolutions.com>
2024-02-05 12:54:29 -0500

    This commit introduces custom actions, 'WriteFile' and 'CleanFile', in the newly added 'CustomActions.wxs' Wix file. These actions are included in the 'InstallExecuteSequence' to execute during the installation process. In addition, the project setup is updated, and HelloWorldCustomAction project is referenced in 'HelloWorldSetup.wixproj'. The project reference is also added to the solution file. Further, the HelloWorld Application is updated to read and display the content of 'CustomAction.txt', if it exists, in its console output.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace HelloWorld;
 
@@ -7,6 +8,12 @@ internal static class Program
     public static void Main()
     {
         Console.WriteLine("Hello World.");
+        Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        Console.WriteLine(Environment.CurrentDirectory);
+        if (File.Exists("CustomAction.txt"))
+        {
+            Console.WriteLine(File.ReadAllText("CustomAction.txt"));
+        }
         Console.WriteLine("Press any key to continue.");
         Console.ReadKey();
     }
